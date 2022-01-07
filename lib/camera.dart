f18d9141d25e5a71069223715aae80846532e4dc
gracentruth <63465350+gracentruth@users.noreply.github.com>
2022-01-07 11:02:43 +0900

    Initial commit

---
+++ lib/camera.dart
@@ -0,0 +1,151 @@
+
+
+import 'package:flutter/material.dart';
+import 'package:camera/camera.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:tflite/tflite.dart';
+import 'dart:math' as math;
+
+import 'package:image/image.dart' as imglib;
+
+import 'models.dart';
+
+typedef void Callback(List<dynamic> list, int h, int w);
+
+class Camera extends StatefulWidget {
+  final List<CameraDescription> cameras;
+  final Callback setRecognitions;
+  final String model;
+
+  Camera(this.cameras, this.model, this.setRecognitions);
+
+  @override
+  _CameraState createState() => new _CameraState();
+}
+
+class _CameraState extends State<Camera> {
+  CameraController controller;
+  bool isDetecting = false;
+  //File imageFile = null;
+
+  Future<List<int>> convertImagetoPng(CameraImage image) async {
+    try {
+      imglib.Image img;
+      if (image.format.group == ImageFormatGroup.yuv420) {
+        img = _convertYUV420(image);
+      } else if (image.format.group == ImageFormatGroup.bgra8888) {
+        img = _convertBGRA8888(image);
+      }
+
+      imglib.PngEncoder pngEncoder = new imglib.PngEncoder();
+
+      // Convert to png
+      List<int> png = pngEncoder.encodeImage(img);
+      return png;
+    } catch (e) {
+      print(">>>>>>>>>>>> ERROR:" + e.toString());
+    }
+    return null;
+  }
+
+  imglib.Image _convertBGRA8888(CameraImage image) {
+    return imglib.Image.fromBytes(
+      image.width,
+      image.height,
+      image.planes[0].bytes,
+      format: imglib.Format.bgra,
+    );
+  }
+
+// CameraImage YUV420_888 -> PNG -> Image (compresion:0, filter: none)
+// Black
+  imglib.Image _convertYUV420(CameraImage image) {
+    var img = imglib.Image(image.width, image.height); // Create Image buffer
+
+    Plane plane = image.planes[0];
+    const int shift = (0xFF << 24);
+
+    // Fill image buffer with plane[0] from YUV420_888
+    for (int x = 0; x < image.width; x++) {
+      for (int planeOffset = 0;
+      planeOffset < image.height * image.width;
+      planeOffset += image.width) {
+        final pixelColor = plane.bytes[planeOffset + x];
+        // color: 0x FF  FF  FF  FF
+        //           A   B   G   R
+        // Calculate pixel color
+        var newVal = shift | (pixelColor << 16) | (pixelColor << 8) | pixelColor;
+
+        img.data[planeOffset + x] = newVal;
+      }
+    }
+
+    return img;
+  }
+
+
+  @override
+  void initState() {
+    super.initState();
+
+    if (widget.cameras == null || widget.cameras.length < 1) {
+      print('No camera is found');
+    } else {
+      controller = new CameraController(
+        widget.cameras[0],
+        ResolutionPreset.high,
+      );
+      controller.initialize().then((_) {
+        if (!mounted) {
+          return;
+        }
+        setState(() {});
+
+        controller.startImageStream((CameraImage img) {
+
+         dynamic image= convertImagetoPng(img);
+
+
+
+
+
+
+
+
+        });
+      });
+    }
+  }
+
+  @override
+  void dispose() {
+    controller?.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    if (controller == null || !controller.value.isInitialized) {
+      return Container();
+    }
+
+    var tmp = MediaQuery.of(context).size;
+    var screenH = math.max(tmp.height, tmp.width);
+    var screenW = math.min(tmp.height, tmp.width);
+    tmp = controller.value.previewSize;
+    var previewH = math.max(tmp.height, tmp.width);
+    var previewW = math.min(tmp.height, tmp.width);
+    var screenRatio = screenH / screenW;
+    var previewRatio = previewH / previewW;
+
+    return OverflowBox(
+      maxHeight:
+          screenRatio > previewRatio ? screenH : screenW / previewW * previewH,
+      maxWidth:
+          screenRatio > previewRatio ? screenH / previewH * previewW : screenW,
+      child: CameraPreview(controller),
+    );
+  }
+}
+
+//********************** IMAGE PICKER
